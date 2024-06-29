e91ec7f74c60bb77964c1ca425c459e54835407d
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-29 22:18:45 +0200

    Add Weather Component to wrapper all Weather Content

---
+++ src/components/Weather/WeatherContainer.tsx
@@ -0,0 +1,11 @@
+const WeatherContainer = () => {
+  return (
+    <main className="mt-3 px-2">
+      <div className="container mx-auto flex flex-col lg:flex-row gap-8">
+        WeatherContainer
+      </div>
+    </main>
+  );
+};
+
+export default WeatherContainer;
