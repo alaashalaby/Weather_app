229ed9d9aafbbf43923f08961e5ec5b3cc0382c0
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-29 22:12:38 +0200

    implement Theme Toggle functionally & save theme in localStorage

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 const Layout = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  useEffect(() => {
+    const themeFromLocalStorage = localStorage.getItem("theme");
+    if (themeFromLocalStorage === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    if (!isDarkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+
+      localStorage.setItem("theme", "light");
+    }
+  };
   return (
     <>
-      <Header />
+      <Header handleToggleTheme={handleToggleTheme} isDarkMode={isDarkMode} />
     </>
   );
 };
