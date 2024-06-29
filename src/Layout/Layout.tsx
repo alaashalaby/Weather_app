a7eb75d0f88afdc37a61801518ab3226202564dd
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-29 22:57:19 +0200

    move function of Toggle theme to Header Component

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,32 +1,9 @@
-import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import WeatherContainer from "../components/Weather/WeatherContainer";
 const Layout = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  useEffect(() => {
-    const themeFromLocalStorage = localStorage.getItem("theme");
-    if (themeFromLocalStorage === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      setIsDarkMode(false);
-      document.documentElement.classList.remove("dark");
-    }
-  }, []);
-  const handleToggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    if (!isDarkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-
-      localStorage.setItem("theme", "light");
-    }
-  };
   return (
     <div className="bg-linear-light dark:bg-linear-dark pb-2">
-      <Header handleToggleTheme={handleToggleTheme} isDarkMode={isDarkMode} />
+      <Header/>
       <WeatherContainer />
     </div>
   );
