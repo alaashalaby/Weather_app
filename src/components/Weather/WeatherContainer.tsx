002e4db669360ab93e6336354e0c44018bd8ada4
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-29 22:33:05 +0200

    Add all components to Weather Container

--- src/components/Weather/WeatherContainer.tsx
+++ src/components/Weather/WeatherContainer.tsx
@@ -1,8 +1,20 @@
+import CurrentWeatherItem from "../CurrentWeather/CurrentWeatherItem";
+import WeatherForecastContainer from "../Forecast/WeatherForecastContainer";
+import WeatherDetailsContainer from "../WeatherDetails/WeatherDetailsContainer";
+import WeatherHourlyContainer from "../WeatherHourly/WeatherHourlyContainer";
+
 const WeatherContainer = () => {
   return (
     <main className="mt-3 px-2">
       <div className="container mx-auto flex flex-col lg:flex-row gap-8">
-        WeatherContainer
+        <div>
+          <CurrentWeatherItem />
+          <WeatherForecastContainer />
+        </div>
+        <div className="flex-1">
+          <WeatherDetailsContainer />
+          <WeatherHourlyContainer />
+        </div>
       </div>
     </main>
   );
