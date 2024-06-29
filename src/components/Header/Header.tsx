eb763149f04137e051e72dc1b102f1638636ea32
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-29 23:01:00 +0200

    Add Function of toggle theme to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HeaderIcons from "./HeaderIcons";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-const Header = ({
-  handleToggleTheme,
-  isDarkMode,
-}: {
-  handleToggleTheme: () => void;
-  isDarkMode: boolean;
-}) => {
+const Header = () => {
   const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    useEffect(() => {
+      const themeFromLocalStorage = localStorage.getItem("theme");
+      if (themeFromLocalStorage === "dark") {
+        setIsDarkMode(true);
+        document.documentElement.classList.add("dark");
+      } else {
+        setIsDarkMode(false);
+        document.documentElement.classList.remove("dark");
+      }
+    }, []);
+    const handleToggleTheme = () => {
+      setIsDarkMode(!isDarkMode);
+      if (!isDarkMode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+
+        localStorage.setItem("theme", "light");
+      }
+    };
   return (
     <header className="bg-light-150 dark:bg-dark-primary pr-2 py-2 md:px-3 md:py-0 sticky inset-0 z-50 shadow-sm">
       <div className="container mx-auto flex justify-between items-center relative">
