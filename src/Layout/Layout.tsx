6923ca0da47536febaefe53eef005af4b6d8fdb1
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-30 20:44:33 +0200

    Manage state for airPollution & forecast & Implement function get weather using CurrentLocation

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -4,9 +4,16 @@ import WeatherContainer from "../components/Weather/WeatherContainer";
 import axiosInstance from "../api/axiosInstance";
 import NoData from "../components/NoData";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { fetchWeatherByCoordinates } from "../api/geoLocation";
+import { fetchAirPollutionData } from "../api/fetchAirPollution";
+import { fetchFiveDayForecast } from "../api/fetchForecastData";
 const Layout = () => {
   const [cityName, setCityName] = useState<string>("");
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [airPollution, setAirPollution] = useState<AirPollutionData | null>(
+    null
+  );
+  const [forecastData, setForecastData] = useState<ForecastData | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
   const apiKey = import.meta.env.VITE_API_KEY as string;
@@ -19,6 +26,18 @@ const Layout = () => {
           `/weather?q=${cityName}&appid=${apiKey}`
         );
         setWeatherData(response);
+        if (response.coord) {
+          const airPollutionResponse = await fetchAirPollutionData(
+            response.coord.lat,
+            response.coord.lon
+          );
+          setAirPollution(airPollutionResponse);
+          const forecastResponse = await fetchFiveDayForecast(
+            response.coord.lat,
+            response.coord.lon
+          );
+          setForecastData(forecastResponse);
+        }
         setIsLoading(false);
       } catch (error: any) {
         setError(
@@ -26,6 +45,8 @@ const Layout = () => {
             "Failed to fetch weather data, Please try again later"
         );
         setWeatherData(null);
+        setAirPollution(null);
+        setForecastData(null);
       } finally {
         setIsLoading(false);
       }
@@ -42,15 +63,70 @@ const Layout = () => {
     setCityName(cityNameValue);
     if (cityNameValue.trim() === "") {
       setWeatherData(null);
+      setAirPollution(null);
+      setForecastData(null);
       setError("");
+    }
+  };
+  const handleGetCurrentLocation = async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          setIsLoading(true);
+          setError("");
+          try {
+            const response = await fetchWeatherByCoordinates(
+              position.coords.latitude,
+              position.coords.longitude
+            );
+            setWeatherData(response);
+            if (response.coord) {
+              const airPollutionResponse = await fetchAirPollutionData(
+                response.coord.lat,
+                response.coord.lon
+              );
+              setAirPollution(airPollutionResponse);
+              const forecastResponse = await fetchFiveDayForecast(
+                response.coord.lat,
+                response.coord.lon
+              );
+              setForecastData(forecastResponse);
+            }
+            setIsLoading(false);
+          } catch (error: any) {
+            setError(
+              error.message ||
+                "Failed to fetch weather data, Please try again later"
+            );
+            setWeatherData(null);
+          } finally {
+            setIsLoading(false);
+          }
+        },
+        (error) => {
+          setError("Failed to get Current Location");
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser");
     }
   };
   return (
     <div className="bg-linear-light dark:bg-linear-dark pb-2 min-h-screen">
-      <Header cityName={cityName} handleCityNameChange={handleCityNameChange} />
-      {!isLoading && !cityName && <NoData />}
+      <Header
+        cityName={cityName}
+        handleCityNameChange={handleCityNameChange}
+        handleGetCurrentLocation={handleGetCurrentLocation}
+      />
+      {!isLoading && !weatherData && !cityName && <NoData />}
       {isLoading && <LoadingSpinner />}
-      {weatherData && <WeatherContainer weatherData={weatherData} />}
+      {weatherData && (
+        <WeatherContainer
+          weatherData={weatherData}
+          airPollution={airPollution}
+          forecastData={forecastData}
+        />
+      )}
       {error && (
         <p className="text-red-500 text-center mt-7 text-3xl">{error}</p>
       )}
