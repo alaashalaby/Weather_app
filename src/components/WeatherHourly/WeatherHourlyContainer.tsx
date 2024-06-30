50702b3bb07ef19fa6453cc1930c0af5404e445a
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-30 21:19:03 +0200

    Add hourlyData Array and Wrap WeatherHourlyItem inside it

--- src/components/WeatherHourly/WeatherHourlyContainer.tsx
+++ src/components/WeatherHourly/WeatherHourlyContainer.tsx
@@ -8,15 +8,17 @@ const WeatherHourlyContainer = ({
   const todayForecastData = forecastData
     ? filterTodayForecastData(forecastData)
     : [];
+const hourlyData = todayForecastData.slice(0, 8);
   return (
     <div>
       <p className="text-2xl my-4 text-light-350 dark:text-dark-accent">
         Today at
       </p>
-      <div className="flex flex-col md:flex-row gap-5 rounded-xl bg-light-50 dark:bg-dark-secondary p-4 shadow-md">
-       {todayForecastData&& todayForecastData.map((item,index) => (
-         <WeatherHourlyItem key={index} forecast={ item} />
-       ))}
+      <div className="flex flex-col md:flex-row gap-4 lg:gap-2 rounded-xl bg-light-50 dark:bg-dark-secondary p-4 shadow-md">
+        {hourlyData &&
+          hourlyData.map((item, index) => (
+            <WeatherHourlyItem key={index} forecast={item} />
+          ))}
       </div>
     </div>
   );
