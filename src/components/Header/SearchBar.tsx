4d0a12d6a0af0f1696415567b69cef930c522e27
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-30 06:45:02 +0200

    Prevent default behavior for form & Added cityName , handleCityNameChange for input to manage input changes

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,7 +1,17 @@
 import { BiSearch } from "react-icons/bi";
-const SearchBar = ({ isSearchBarVisible }: { isSearchBarVisible: boolean }) => {
+type SearchBarProps = {
+  isSearchBarVisible: boolean;
+  cityName: string;
+  handleCityNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const SearchBar = ({
+  isSearchBarVisible,
+  cityName,
+  handleCityNameChange,
+}:SearchBarProps) => {
   return (
     <form
+      onSubmit={(e)=>e.preventDefault()}
       className={`${
         isSearchBarVisible ? "flex" : "hidden"
       } md:flex items-center flex-grow md:flex-grow-0 mx-2`}
@@ -16,6 +26,8 @@ const SearchBar = ({ isSearchBarVisible }: { isSearchBarVisible: boolean }) => {
         <input
           type="text"
           placeholder="Search by City Name.."
+          value={cityName}
+          onChange={handleCityNameChange}
           className="w-full rounded-full text-light-350 dark:text-dark-accent  p-2 placeholder:text-sm placeholder:text-light-400 placeholder:dark:text-gray-400"
           aria-label="City Search"
         />
