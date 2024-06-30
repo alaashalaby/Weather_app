772724da5c46274981002e267f96ab3fc0c10231
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-30 06:49:25 +0200

    Added weatherData as a props to components

--- src/components/WeatherDetails/WeatherDetailsContainer.tsx
+++ src/components/WeatherDetails/WeatherDetailsContainer.tsx
@@ -2,17 +2,17 @@ import WeatherAirQuality from "./WeatherAirQuality"
 import WeatherInfoContainer from "./WeatherInfoContainer";
 import WeatherSunriseSunset from "./WeatherSunriseSunset"
 
-const WeatherDetailsContainer = () => {
+const WeatherDetailsContainer = ({weatherData}: {weatherData: WeatherData}) => {
   return (
     <div className="bg-light-50 dark:bg-dark-secondary text-dark-accent shadow-md rounded-xl p-5">
       <h1 className="text-xl mb-2 text-light-350 dark:text-dark-accent">
         Todays Highlights
       </h1>
       <div className="flex flex-col md:flex-row gap-2 ">
-        <WeatherAirQuality />
-        <WeatherSunriseSunset />
+        <WeatherAirQuality/>
+        <WeatherSunriseSunset weatherData={weatherData} />
       </div>
-      <WeatherInfoContainer />
+      <WeatherInfoContainer weatherData={weatherData} />
     </div>
   );
 }
