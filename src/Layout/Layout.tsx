5bac2c7672df37783653f711ca6a7f1de4fae27f
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-01 21:48:01 +0200

    Refactor Weather data fetching and error handling in Layout Component to improve code organization and readabilty

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import WeatherContainer from "../components/Weather/WeatherContainer";
-import axiosInstance from "../api/axiosInstance";
 import NoData from "../components/NoData";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import { fetchWeatherByCoordinates } from "../api/geoLocation";
 import { fetchAirPollutionData } from "../api/fetchAirPollution";
 import { fetchFiveDayForecast } from "../api/fetchForecastData";
 import { AxiosError } from "axios";
+import { fetchWeatherByCityName } from "../api/fetchWeatherByCity";
 const Layout = () => {
   const [cityName, setCityName] = useState<string>("");
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
@@ -19,54 +19,49 @@ const Layout = () => {
   const [error, setError] = useState<string>("");
   const apiKey = import.meta.env.VITE_API_KEY as string;
   useEffect(() => {
-    const fetchWeatherByCityName = async (cityName: string) => {
+    const fetchData = async () => {
+      if (!cityName.trim()) return;
       setIsLoading(true);
       setError("");
       try {
-        const response = await axiosInstance.get<never, WeatherData>(
-          `/weather?q=${cityName}&appid=${apiKey}`
-        );
-        setWeatherData(response);
-        if (response.coord) {
-          const airPollutionResponse = await fetchAirPollutionData(
-            response.coord.lat,
-            response.coord.lon
-          );
+        const weatherResponse = await fetchWeatherByCityName(cityName, apiKey);
+        setWeatherData(weatherResponse);
+        const { coord } = weatherResponse;
+        if (coord) {
+          const [airPollutionResponse, forecastResponse] = await Promise.all([
+            fetchAirPollutionData(coord.lat, coord.lon),
+            fetchFiveDayForecast(coord.lat, coord.lon),
+          ]);
           setAirPollution(airPollutionResponse);
-          const forecastResponse = await fetchFiveDayForecast(
-            response.coord.lat,
-            response.coord.lon
-          );
           setForecastData(forecastResponse);
         }
         setIsLoading(false);
-      } catch (error: AxiosError | unknown) {
-        if (error instanceof AxiosError) {
-          setError(
-            error.message ||
-              "Failed to fetch weather data, Please try again later"
-          );
-        } else {
-          setError("An unknown error occurred");
-        }
-        setWeatherData(null);
-        setAirPollution(null);
-        setForecastData(null);
+      } catch (error) {
+        handleError(error);
       } finally {
         setIsLoading(false);
       }
     };
-    const debounce = setTimeout(() => {
-      if (cityName.trim() !== "") {
-        fetchWeatherByCityName(cityName);
-      }
-    }, 700);
-    return () => clearTimeout(debounce);
+    const debounceTimer = setTimeout(fetchData, 700);
+    return () => clearTimeout(debounceTimer);
   }, [cityName, apiKey]);
+  const handleError = (error: AxiosError | unknown) => {
+    if (error instanceof AxiosError) {
+      setError(
+        error.message || "Failed to fetch weather data. Please try again later"
+      );
+    } else {
+      setError("An unknown error occurred");
+    }
+    setWeatherData(null);
+    setAirPollution(null);
+    setForecastData(null);
+    setError("");
+  };
   const handleCityNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const cityNameValue = e.target.value;
     setCityName(cityNameValue);
-    if (cityNameValue.trim() === "") {
+    if (!cityNameValue.trim()) {
       setWeatherData(null);
       setAirPollution(null);
       setForecastData(null);
@@ -112,7 +107,7 @@ const Layout = () => {
             setIsLoading(false);
           }
         },
-        (error ) => {
+        (error) => {
           setError("Failed to get Current Location" || error.message);
         }
       );
@@ -129,15 +124,13 @@ const Layout = () => {
       />
       {!isLoading && !weatherData && !cityName && <NoData />}
       {isLoading && <LoadingSpinner />}
-      {weatherData &&
-        airPollution &&
-        forecastData&&(
-          <WeatherContainer
-            weatherData={weatherData}
-            airPollution={airPollution}
-            forecastData={forecastData}
-          />
-        )}
+      {weatherData && airPollution && forecastData && (
+        <WeatherContainer
+          weatherData={weatherData}
+          airPollution={airPollution}
+          forecastData={forecastData}
+        />
+      )}
       {error && (
         <p className="text-red-500 text-center mt-7 text-3xl">{error}</p>
       )}
